cb7cabd1094a388a2467115ee100d0ee95631840
PeteChicchetti <pj0524@gmail.com>
2022-10-21 18:53:51 -0400

    added missing ,

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ const questions = [
         choices: ['None', 'Apache License 2.0', 'GNU General Public License v3.0', 'MIT License', 'BSD 2-Clause Simplified License', 
                     'BSD 3-Clause "New" or "Revised" License', 'Boost Software License 1.0', 'Creative Commons Zero v1.0 Universal', 
                     'Eclipse Public License 2.0', 'GNU Affero General Public License v3.0', 'GNU General Public License v2.0',
-                    'GNU Lesser General Public License v2.1', 'Mozilla Public License 2.0', 'The Unlicense']
+                    'GNU Lesser General Public License v2.1', 'Mozilla Public License 2.0', 'The Unlicense'],
         validate: (value) => {if(value){return true} else {return 'i need a value to contine'}}
       },
       {
